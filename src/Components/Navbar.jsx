24189cb250b20592dba3763a19f822f41808abc6
kppppppp <krish25@somaiya.edu>
2025-11-26 05:59:45 +0530

    Install bootstrap

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min.js";
+
 import logo from "./assets/logo.png";
 import { Home, Info, Images, Phone } from "lucide-react";
 function Navbar(){
